92a198c3df668e57a3aa5711f0061af66957a8a1
Aleksandar Kljaic <Hexers@users.noreply.github.com>
2020-11-15 00:16:03 -0600

    Update MainGameLoop.java

--- MainGameLoop.java
+++ MainGameLoop.java
@@ -2,20 +2,51 @@ package engineTester;
 
 import org.lwjgl.opengl.Display;
 import renderEngine.DisplayManager;
+import renderEngine.Loader;
+import renderEngine.RawModel;
+import renderEngine.Renderer;
 
 public class MainGameLoop
 {
-    public static void main(String[] args)
-    {
+    /**
+     * Creates the display and continuously updates the display until user closes.
+     * @param args
+     *
+     * https://www.youtube.com/watch?v=z2yFlvkBbmk&list=PLRIWtICgwaX0u7Rf9zkZhLoLuZVfUksDP&index=3
+     *
+     * @author Hexers
+     */
+    public static void main(String[] args) {
         DisplayManager.createDisplay();
+
+        Loader loader = new Loader();
+        Renderer renderer = new Renderer();
+
+        // OpenGL expects verticies to be defined counter clockwise by default
+        float[] verticies =
+                {
+                        // Left bottom triangle
+                        -0.5f, 0.5f, 0f,
+                        -0.5f, -0.5f, 0f,
+                        -0.5f, -0.5f, 0f,
+                        // Right top triangle
+                        0.5f, -0.5f, 0f,
+                        0.5f, 0.5f, 0f,
+                        -0.5f, 0.5f, 0f
+                };
+
+        RawModel model = loader.loadToVAO(verticies);
+
 
         while(!Display.isCloseRequested())
         {
+            renderer.prepare();
             // game logic
-            // render
+            renderer.render(model);
             DisplayManager.updateDisplay();
         }
 
+        loader.cleanUp();
         DisplayManager.closeDisplay();
     }
 }
